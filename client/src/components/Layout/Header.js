e978934ba14ecc14773c542cf3776b024d90ce63
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-04 18:19:31 +0200

    navbar and footer added with some css style

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              <HiShoppingBag /> Ecommerce App by Pranab
+              <HiShoppingBag /> 🛒 Ecommerce App by Pranab
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">

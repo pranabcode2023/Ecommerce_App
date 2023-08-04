f3dce09493229ab7f8c837afd7d57e83bb5ed262
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-04 15:05:30 +0200

    middleware function added to check token and to check admin and tested with postman

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,13 +1,49 @@
-import JWT from 'jsonwebtoken'
+import JWT from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
-//protect routes token base
+//protect routes token base/this middleware  check for token
 
 export const requireSignIn = async (req, res, next) => {
-    try {
-        const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)  // jwt verify fuction 
-        next()
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    // jwt verify fuction
+    const decode = JWT.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decode;
 
-}
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      success: false,
+      error,
+      message: "Error in token  middelware",
+    });
+  }
+};
+
+//admin access/this middleware  check for role /admin
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.user._id);
+    if (user.role !== 1) {
+      // Number 1 stand for admin
+      return res.status(401).send({
+        success: false,
+        message: "Unauthorized Access",
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+
+    res.status(401).send({
+      success: false,
+      error,
+      message: "Error in admin middelware",
+    });
+  }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,25 @@
 import express from "express";
 
-import { registerController, loginController, testController, } from '../controllers/authController.js'
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
 //router object
 
-const router = express.Router()
+const router = express.Router();
 
 //routing
 
 //Register|| POST METHOD
-router.post('/register', registerController)
+router.post("/register", registerController);
 
 //Login||  POST METHOD
-router.post('/login', loginController)
+router.post("/login", loginController);
 
-router.get('/test', testController)
+router.get("/test", requireSignIn, isAdmin, testController);
 
 export default router;

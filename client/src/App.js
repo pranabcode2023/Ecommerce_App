08aa3ca91d1a550e4d98b0ae52d53757638ff6ef
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-04 16:17:29 +0200

    added layout-js, header.js , footer.js filer in layout folder inside components folder and install react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Layout from "./components/Layout/Layout.js";
 function App() {
   return (
     <>
-      <h1>Ecommerce App</h1>
+      <Layout>
+        <h1>Ecommerce App</h1>
+      </Layout>
     </>
   );
 }

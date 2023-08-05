1af0f35ea052b027ba61fce8bba7f9b76e32b67b
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-05 09:23:40 +0200

    simple code to make SE friendly using  react helmet

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-
-const Layout = ({ children }) => {
+import { Helmet } from "react-helmet";
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>{children}</main>
       <Footer />
@@ -12,4 +20,10 @@ const Layout = ({ children }) => {
   );
 };
 
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react, node, mongodb",
+  author: "me",
+};
 export default Layout;

4f00b58009561300842ab08b0093f28cd37d5d1f
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-05 11:27:43 +0200

    installed react toastify to get pop up message and import in  app.js and Layout

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,10 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
-
+import { toast } from "react-toastify";
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  // form function
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, phone, address);
+    toast.success("register successfully"); // toast pop up from react toastify
+  };
+
   return (
     <Layout title="Register- Ecommerce App">
-      <div className="register"></div>
+      <div className="register">
+        <h1>Register Page</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)} // on change event
+              className="form-control"
+              id="exampleInputName"
+              placeholder="Enter your Name"
+              required //  validation
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Phone no."
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <textarea
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Address"
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };

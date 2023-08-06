c3f1837f1fb1ed8c2635f55b907d3c34fe833703
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-06 13:14:28 +0200

    all changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "../src/styles/AuthStyles.css";
+// import "../src/styles/AuthStyles.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 
   // <BrowserRouter>
   //   <React.StrictMode>

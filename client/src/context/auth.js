491025a886eff2fa1d854e10c01831f49e467d19
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-07 10:45:29 +0200

    inside useEffect use JSON.parse function to get data  from local storage

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,4 +1,4 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useEffect, useContext, createContext } from "react";
 
 const AuthContext = createContext();
 
@@ -7,7 +7,17 @@ const AuthProvider = ({ children }) => {
     user: null,
     token: "",
   });
-
+  useEffect(() => {
+    const data = localStorage.getItem("auth");
+    if (data) {
+      const parseData = JSON.parse(data); // used function JSON.parse to parse the data
+      setAuth({
+        ...auth,
+        user: parseData.user,
+        token: parseData.token,
+      });
+    }
+  }, [auth]);
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}

5ce9c7be26da48e38dd4b6d7d915777b40ae5e6a
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-08 10:14:47 +0200

    added another route "Dashboard " added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dasboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />

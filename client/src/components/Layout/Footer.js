132abccfb52a8275789aea23702a97440bb0125f
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-08 10:09:02 +0200

    heading edited

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h4 className="text-center">All Right Reserved &copy; me</h4>
+      <h4 className="text-center">All Right Reserved &copy; Pranab</h4>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>

186c94b170bf62b1ae8251e83747f4eea3394528
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-08 10:22:23 +0200

    added new proted route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -20,6 +20,12 @@ router.post("/register", registerController);
 //Login||  POST METHOD
 router.post("/login", loginController);
 
+//test routes
 router.get("/test", requireSignIn, isAdmin, testController);
+
+// protected route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
 export default router;

7d8c5fd2e2b2cc5f159bb4e53a0f70aea647c230
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-10 00:22:42 +0200

    added div and import adminmenu

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
 
 const CreateProduct = () => {
   return (
-    <div>
-      <h1> CreateProduct</h1>
-    </div>
+    <Layout title={"Daahboard-Create Product"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>Create Product</h1>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 

07232326bcedc9b64a2691b81bcc08f99b9494bd
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-09 15:19:16 +0200

    import forgotPasswordController

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,6 +4,7 @@ import {
   registerController,
   loginController,
   testController,
+  forgotPasswordController,
 } from "../controllers/authController.js";
 
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";

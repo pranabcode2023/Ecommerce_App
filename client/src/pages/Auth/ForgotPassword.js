bffa5bc3bca9319ff46dae6dd337aeb843a25d40
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-09 15:59:20 +0200

    added page for named as ForgotPassword.js inside  pages/Auth folder and added react arrow function export component

---
+++ client/src/pages/Auth/ForgotPassword.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout.js";
+const ForgotPassword = () => {
+  return (
+    <Layout>
+      <h1>Forgot Password</h1>
+    </Layout>
+  );
+};
+
+export default ForgotPassword;

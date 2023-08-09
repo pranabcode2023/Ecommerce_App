0e136325adb5da154281f411a9b96cbd7088ce57
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-09 15:09:33 +0200

    added question added in user schema to exucute forget password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,44 +1,46 @@
 import mongoose from "mongoose";
 
-
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true, // must have name
-        trim: true, // to remove white space
+      type: String,
+      required: true, // must have name
+      trim: true, // to remove white space
     },
     email: {
-        type: String,
-        required: true, // must have email
-        unique: true,   // one email one user
+      type: String,
+      required: true, // must have email
+      unique: true, // one email one user
     },
 
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
-
     phone: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     address: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
-    role: {   // it can possible  with Boolean or Number/ i used number
-        type: Number,
-        default: 0   // 0 means false/ 1 means true
-    }
+    question: {
+      type: String,
+      required: true,
+    },
 
-}, { timestamps: true });  // timestams created time added for new user
-
-
-
+    role: {
+      // it can possible  with Boolean or Number/ i used number
+      type: Number,
+      default: 0, // 0 means false/ 1 means true
+    },
+  },
+  { timestamps: true }
+); // timestams created time added for new user
 
 const userModel = mongoose.model("users", userSchema);
 // "users"  : from MongoDB database, userSchema : created informaton for user from me .

d0acfceed97fd82fc4773fedd402a0104fbab3ec
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-09 15:39:14 +0200

    wrote forgotPasswordController function with validation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -124,6 +124,58 @@ export const loginController = async (req, res) => {
   }
 };
 
+// forgotPasswordController
+
+export const forgotPasswordController = async (req, res) => {
+  try {
+    const { email, answer, newPassword } = req.body;
+
+    //validation
+    if (!email) {
+      res.status(400).send({
+        message: "Email is required",
+      });
+    }
+
+    if (!answer) {
+      res.status(400).send({
+        message: "Answer is required",
+      });
+    }
+
+    if (!newPassword) {
+      res.status(400).send({
+        message: "New Password is required",
+      });
+    }
+
+    // check
+    const user = await userModel.findOne({ email, answer });
+
+    4; //validation
+    if (!user) {
+      return res.status(400).send({
+        success: false,
+        massage: "Wrong Email or Answer",
+      });
+    }
+
+    const hashed = await hashPassword(newPassword);
+    await userModel.findByIdAndUpdate(user._id, { password: hashed });
+    res.status(200).send({
+      success: true,
+      message: "Password reset Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
+
 //testController
 export const testController = (req, res) => {
   try {

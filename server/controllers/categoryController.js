e37b7572034f2a1bf56f9ed712c16645f6bc3228
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-10 11:57:17 +0200

    added function for update Category

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,5 +1,7 @@
 import categoryModel from "../models/categoryModel.js";
 import slugify from "slugify";
+
+// update Category
 export const createCategoryController = async (req, res) => {
   try {
     const { name } = req.body;
@@ -34,6 +36,32 @@ export const createCategoryController = async (req, res) => {
       success: false,
       error,
       message: "Error in  category",
+    });
+  }
+};
+
+// update Category
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Category Updated Successfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while updating category",
     });
   }
 };

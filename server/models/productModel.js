02049e92733b5a815e26ff5f1d56829604bc5b7f
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-12 16:56:09 +0200

    added product Schema

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,7 +1,48 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.schema({});
+const productSchema = new mongoose.schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
 
-const productModel = mongoose.model("products", productSchema);
+    //to get information type + ref get from categoryModel
+    category: {
+      type: mongoose.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+    },
+
+    photo: {
+      type: Buffer,
+      contentType: String,
+    },
+
+    Shipping: {
+      type: Boolean,
+    },
+  },
+  { timestams }
+);
+
+const productModel = mongoose.model("Products", productSchema);
 
 export default productModel;

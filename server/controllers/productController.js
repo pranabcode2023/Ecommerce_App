b0da6de9291899496e7ea2fb6451146dfd7eea7e
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-13 13:15:56 +0200

    import"  fs" = file System

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,8 @@
 import productModel from "./../models/productModel.js";
 
+// fs = file system .its integrated with node , don't need to install extra package
+import fs from "fs";
+
 export const createProductController = async (req, res) => {
   try {
     const products = await productModel;

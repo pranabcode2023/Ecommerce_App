098d395d589824098cf6dc642381d1f0df3c95e8
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-13 13:10:53 +0200

    added fuction for createProductController

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,1 +1,14 @@
-export const createProductController = () => {};
+import productModel from "./../models/productModel.js";
+
+export const createProductController = async (req, res) => {
+  try {
+    const products = await productModel;
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in creating product",
+    });
+  }
+};

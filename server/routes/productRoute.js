9aa7eb748f98560ddca4f9cbcb20668daec73f01
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-13 14:02:52 +0200

    added route to get all products

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "./../middlewares/authMiddleware.js";
-import { createProductController } from "../controllers/productController.js";
+import {
+  createProductController,
+  getProductController,
+} from "../controllers/productController.js";
 import formidable from "express-formidable";
 
 const router = express.Router();
@@ -13,4 +16,6 @@ router.post(
   createProductController
 );
 
+// get products
+router.get("/get-product", getProductController);
 export default router;

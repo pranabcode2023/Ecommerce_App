b62c1724a2af8abc00dbdd891eb7f1ad4c3b8912
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-13 13:20:56 +0200

    destructure productModel

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,6 +5,13 @@ import fs from "fs";
 
 export const createProductController = async (req, res) => {
   try {
+    // to get other information except photo from product schema
+    const { name, slug, description, price, category, quantity, Shipping } =
+      req.fields;
+
+    // to get photo from product schema
+    const { photo } = req.files;
+
     const products = await productModel;
   } catch (error) {
     console.log(error);

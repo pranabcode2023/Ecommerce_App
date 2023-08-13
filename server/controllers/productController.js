ec2fcd3abc3c4e36666bb50c53c9d25b073d363d
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-13 13:36:30 +0200

    added validation used switch and import slugify

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,4 +1,6 @@
+import slugify from "slugify";
 import productModel from "./../models/productModel.js";
+import slugify from "slugify";
 
 // fs = file system .its integrated with node , don't need to install extra package
 import fs from "fs";
@@ -6,13 +8,35 @@ import fs from "fs";
 export const createProductController = async (req, res) => {
   try {
     // to get other information except photo from product schema
-    const { name, slug, description, price, category, quantity, Shipping } =
+    const { name, slug, description, price, category, quantity, shipping } =
       req.fields;
 
     // to get photo from product schema
     const { photo } = req.files;
+    // validation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "Name is required" });
+      case !description:
+        return res.status(500).send({ error: "Description is required" });
+      case !price:
+        return res.status(500).send({ error: "Price is required" });
+      case !category:
+        return res.status(500).send({ error: "Category is required" });
+      case !quantity:
+        return res.status(500).send({ error: "Quantity is required" });
 
-    const products = await productModel;
+      case photo && photo.size > 1000000:
+        return res
+          .status(500)
+          .send({ error: "Photo is required and should be 1MB" });
+    }
+
+    const products = new productModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.contentType = photo.type;
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send({

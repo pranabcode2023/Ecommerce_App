a5a3bce2d84edeb1c6cd0051b8802a2ad58cd26b
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-14 14:24:41 +0200

    added fuction to get photo

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -105,3 +105,24 @@ export const getSingleProductController = async (req, res) => {
     });
   }
 };
+
+// get photo
+
+export const productPhotoController = async (req, res) => {
+  try {
+    const product = await productModel
+      .findById(req.paramas.pid)
+      .select("photo");
+    if (product.photo.data) {
+      res.set("content-type", product.photo.contentType);
+      return res.status(200).send(product.photo.data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while getting photo",
+      error,
+    });
+  }
+};

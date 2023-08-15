d3764783ace3052cd8aa804c6eec4c71dc3ddfee
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-15 16:56:08 +0200

    added route for delete product

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -26,4 +26,7 @@ router.get("/get-product/:slug", getSingleProductController);
 
 // get photo
 router.get("/product-photo/:pid", productPhotoController);
+
+// delete product
+router.delete("/product", deleteroductController);
 export default router;

70298f11d6c9b49a0db05b2105ca68c136dd7b95
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-15 17:33:49 +0200

    added function for update product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -142,3 +142,56 @@ export const deleteroductController = async (req, res) => {
     });
   }
 };
+
+// update product controller
+export const updateProductController = async (req, res) => {
+  try {
+    // to get other information except photo from product schema
+    const { name, slug, description, price, category, quantity, shipping } =
+      req.fields;
+
+    // to get photo from product schema
+    const { photo } = req.files;
+    // validation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "Name is required" });
+      case !description:
+        return res.status(500).send({ error: "Description is required" });
+      case !price:
+        return res.status(500).send({ error: "Price is required" });
+      case !category:
+        return res.status(500).send({ error: "Category is required" });
+      case !quantity:
+        return res.status(500).send({ error: "Quantity is required" });
+
+      case photo && photo.size > 1000000:
+        return res
+          .status(500)
+          .send({ error: "Photo is required and should be 1MB" });
+    }
+
+    const products = await productModel.findByIdAndUpdate(
+      req.params.pid,
+      { ...req.fields, slug: slugify(name) },
+      { new: true }
+    );
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(200).send({
+      success: true,
+      message: "Product updated Succesfully ",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in updating product",
+    });
+  }
+};

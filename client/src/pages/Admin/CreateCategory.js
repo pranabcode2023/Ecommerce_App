31f2a28e4a96a0b68c76a76cf5bcb9bd3057b669
pranabcode2023 <pranabcode2023@gmail.com>
2023-08-16 22:59:51 +0200

    added div classname

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -35,7 +35,7 @@ const CreateCategory = () => {
           </div>
           <div className="col-md-9">
             <h1> Manage Category</h1>
-            <div>
+            <div className="w-75">
               <table className="table">
                 <thead>
                   <tr>

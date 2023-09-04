a6886c019dd5c1648e8a659db186584ba0244f48
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-04 22:18:40 +0200

    import Link and card wrap with Link

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -3,6 +3,7 @@ import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "../../components/Layout/Layout";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { Link } from "react-router-dom";
 const Products = () => {
   const [products, setProducts] = useState([]);
 
@@ -32,15 +33,19 @@ const Products = () => {
           </div>
           <div className="col-md-9">
             <h1 className="text-center">All Products List</h1>
-            {products?.map((p) => (
-              <div className="card" style={{ width: "18rem" }} key={p._id}>
-                <img src={p.photo} className="card-img-top" alt={p.name} />
-                <div className="card-body">
-                  <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">{p.description} </p>
-                </div>
-              </div>
-            ))}
+            <div className="d-flex">
+              {products?.map((p) => (
+                <Link key={p._id} to={`/dashboard/admin/product/${p.slug}`}>
+                  <div className="card m-2" style={{ width: "18rem" }}>
+                    <img src={p.photo} className="card-img-top" alt={p.name} />
+                    <div className="card-body">
+                      <h5 className="card-title">{p.name}</h5>
+                      <p className="card-text">{p.description} </p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>

805f971cfe7562d82f698426e6bc49a0c78e6040
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-04 22:01:40 +0200

    import useState, useEffect , added function to get all products using try catch block, used bootstrap card

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -2,8 +2,27 @@ import React, { useState, useEffect } from "react";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "../../components/Layout/Layout";
 import axios from "axios";
-
+import { toast } from "react-hot-toast";
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  //  get all Products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/get-product`
+      );
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  //lifecycle Method
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <Layout>
       <div>
@@ -13,6 +32,15 @@ const Products = () => {
           </div>
           <div className="col-md-9">
             <h1 className="text-center">All Products List</h1>
+            {products?.map((p) => (
+              <div className="card" style={{ width: "18rem" }} key={p._id}>
+                <img src={p.photo} className="card-img-top" alt={p.name} />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description} </p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>

a45ee38a177c11bbc3bc15b3677dfa2301a003e1
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-07 23:48:45 +0200

    card copied from products page to show all products using map function

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import { useAuth } from "../context/auth.js";
+import axios from "axios";
 
 const HomePage = () => {
   const [auth, setAuth] = useAuth();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+
+  //get products
+
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/get-product`
+      );
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <Layout title={" All Products- Best offers"}>
       <div className="row mt-3">
@@ -15,7 +34,23 @@ const HomePage = () => {
         <div className="col-md-9">
           <h1 className="text-center">All Products</h1>
           <div className="d-flex flex-wrap">
-            <h1>products</h1>
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description} </p>
+                  <button className="btn btn-primary ms-1">More Details</button>
+                  <button className="btn btn-secondary ms-1">
+                    ADD TO CART
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>

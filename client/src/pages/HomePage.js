1dbeffe0c316ca57e8ab7ad3de6541834fdf41fa
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-07 23:29:11 +0200

    added div to display all products and also filter for products

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import { useAuth } from "../context/auth.js";
 
 const HomePage = () => {
   const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
   return (
-    <Layout title={"Best offers"}>
-      <h1> HomePage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title={" All Products- Best offers"}>
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <h4 className="text-center">Filter By Category</h4>
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">
+            <h1>products</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };

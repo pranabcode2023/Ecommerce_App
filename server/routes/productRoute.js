5abec2e38637476baf2c5f491eb6805e082912da
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-10 10:09:56 +0200

    added route for earchProductController  to serach product

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -9,6 +9,7 @@ import {
   productFilterController,
   productListController,
   productPhotoController,
+  searchProductController,
   updateProductController,
 } from "../controllers/productController.js";
 import formidable from "express-formidable";
@@ -51,5 +52,8 @@ router.get("/product-count", productCountController);
 
 //  product per page
 router.get("/product-list/:page", productListController);
+
+// Search product
+router.get("/search/:keyword", searchProductController);
 
 export default router;

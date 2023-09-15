7014b6622526f9f8ab55a72f89b2fe734550bb19
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-15 23:20:25 +0200

    card copid from homepage and leavea a note for that

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -8,7 +8,31 @@ const Search = () => {
       <div className='container'>
         <div className='text-center'>
           <h1>Search Results</h1>
-          <h6>{values?.results.length <1?  'No Products Found ':`Found ${values?.results.length}` }</h6>
+          <h6>{values?.results.length <1?  'No Products Found ':`Found ${values?.results.length}`}</h6>
+          {/* //NOTE - this same card copid from homepage. here only map the value.results */}
+          <div className="d-flex flex-wrap mt-4">
+            {values?.results.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">
+                    {/* substring function used to show maximum 30 character  */}
+                    {p.description.substring(0, 30)}...{" "}
+                  </p>
+                  <p className="card-text">€ {p.price} </p>
+                  <button className="btn btn-primary ms-1">More Details</button>
+                  <button className="btn btn-secondary ms-1">
+                    ADD TO CART
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 

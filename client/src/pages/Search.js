76f1494d770c3c7ae2451dbdd7d263bcf05d3dea
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-15 23:14:07 +0200

    added search page and as well as function

---
+++ client/src/pages/Search.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from './../components/Layout/Layout';
+import { useSearch } from '../context/search';
+const Search = () => {
+    const [values, setValues] = useSearch()
+  return (
+    <Layout title={'Search results'}>
+      <div className='container'>
+        <div className='text-center'>
+          <h1>Search Results</h1>
+          <h6>{values?.results.length <1?  'No Products Found ':`Found ${values?.results.length}` }</h6>
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
+
+export default Search

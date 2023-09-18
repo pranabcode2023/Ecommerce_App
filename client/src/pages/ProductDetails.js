934fe894b00ff4d84dd31310a21c7fb2fd3d5639
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-18 23:00:41 +0200

    import axios , useparams and created variable and added function to get product

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Layout from '../components/Layout/Layout'
-
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 const ProductDetails = () => {
+    const params = useParams()
+    const [product, setProduct] = useState({})
+    //intial product details
+    useEffect(()=>{
+        if (params?.slug) getProduct()
+    }, [params?.slug])
+    //get product 
+    const getProduct = async()=> {
+        try {
+            const {data}= await axios.get(
+                `${process.env.REACT_APP_API}/api/v1/product/get-product/${params.slug}`
+              );
+              setProduct(data?.product)
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <Layout>
        <h1>Product Details</h1>
-
+       {/* for checking function working or not  */}
+        {/* {JSON.stringify(product, null,4)} */}
     </Layout>
   )
 }

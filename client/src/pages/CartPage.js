1ac2d9ee4b42b3cb4969c9ba607c60b58f51c549
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-19 21:30:05 +0200

    import context for auth, cart  and div are wrap with Layout and also checking for user log in or not

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,12 +1,35 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
+import { useCart } from "../context/cart";
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
+  const navigate = useNavigate();
+  const [auth, setAuth] = useAuth();
+  const [cart, setCart] = useCart();
+
   return (
     <Layout>
       <div className="container">
         <div className="row">
-          <h1>Your cart</h1>
+          <div className="col-md-12">
+            <h1 className="text-center bg-light p-2 mb-1">
+              {`Hello ${auth?.token && auth?.user.name}`}
+            </h1>
+
+            <h4 className="text-center ">
+              {cart?.length > 1
+                ? `You Have ${cart.length} items in your cart ${
+                    auth?.token ? "" : "Please login to Checkout"
+                  }`
+                : "Your Cart is Empty"}
+            </h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-9"> Cart Item</div>
+          <div className="col-md-3"> checkout | Payment </div>
         </div>
       </div>
     </Layout>

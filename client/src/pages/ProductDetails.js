4512a0f4ffa71d4b226bf7e2bafb870dedfedda8
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-19 14:04:55 +0200

    added product price and  showed in euro currency symbol

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -36,19 +36,22 @@ const ProductDetails = () => {
                       width={'350px'}
                     />
             </div>
-            <div className='col-md-6 '>
-                <h1 className='text-center'>Product Details</h1>
-                <h6>Name: {product.name}</h6>
-                <h6>Description: {product.description}</h6>
-                <h6>Price: {product.price}</h6>
-                <h6>Category: {product.category.name}</h6>
-                {/* //NOTE - later have to fix error  */}
-                {/* <h6>Shipping: {product.shipping}</h6> */}
-                <button className="btn btn-secondary ms-1">
-                    ADD TO CART
-                  </button>
-                </div>
+            <div className="col-md-6 product-details-info">
+          <h1 className="text-center">Product Details</h1>
+          <hr />
+          <h6>Name : {product.name}</h6>
+          <h6>Description : {product.description}</h6>
+          <h6>
+            Price :
+            {product?.price?.toLocaleString("de-EU", {
+              style: "currency",
+              currency: "EUR",
+            })}
+          </h6>
+          <h6>Category : {product?.category?.name}</h6>
+          <button class="btn btn-secondary ms-1">ADD TO CART</button>
         </div>
+      </div>
         <div className='row'>
             Similar Products
         </div>

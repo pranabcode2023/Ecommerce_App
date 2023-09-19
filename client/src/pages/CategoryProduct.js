cdf91e7e207bf1280d2f24e0be1a63f6fbbf38ff
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-19 20:14:55 +0200

    added function to get product category wise

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -1,11 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
-
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 const CategoryProduct = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    if (params?.slug) getProductsByCategory();
+  }, [params?.slug]);
+  const getProductsByCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/product-category/${params.slug}`
+      );
+      setProducts(data?.products);
+      setCategory(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
-      <div className="container">
-        <h1>Category name</h1>
+      <div className="container mt-2">
+        <h4 className="text-center"> Category- {category?.name}</h4>
+        <h6 className="text-center"> {products?.length} result found</h6>
+        <div className="row">
+          <div className="col-md-9">
+            {/* stringify method used for test purpose */}
+            {/* {JSON.stringify(radio, null, 4)} */}
+
+            <div className="d-flex flex-wrap">
+              {products?.map((p) => (
+                <div className="card m-2" style={{ width: "18rem" }}>
+                  <img
+                    src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.name}</h5>
+                    <p className="card-text">
+                      {/* substring function used to show maximum 30 character  */}
+                      {p.description.substring(0, 30)}...{" "}
+                    </p>
+                    <p className="card-text">€ {p.price} </p>
+                    <button
+                      className="btn btn-primary ms-1"
+                      onClick={() => navigate(`/product/${p.slug}`)}
+                    >
+                      More Details
+                    </button>
+                    <button className="btn btn-secondary ms-1">
+                      ADD TO CART
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+            {/* <div className="m-2 p-3">
+              {products && products.length < total && (
+                <button
+                  className="btn btn-warning"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setPage(page + 1);
+                  }}
+                >
+                  {loading ? "loading ..." : "Loadmore"}
+                </button>
+              )}
+            </div> */}
+          </div>
+        </div>
       </div>
     </Layout>
   );

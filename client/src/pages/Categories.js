310c72817f8bb9bd69bb27714a35a587afc00a76
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-19 16:46:40 +0200

    new page for Categories

---
+++ client/src/pages/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+
+const Categories = () => {
+  return (
+    <Layout title={'All Categories'}>
+  <h1></h1>
+    </Layout>
+  )
+}
+
+export default Categories

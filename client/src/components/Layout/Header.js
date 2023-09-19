67f514bc3bf6839d2e578453da563f234fb39917
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-19 16:43:24 +0200

    added dropdown menu for categories from bootstrap and maping function used to show all category

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -47,10 +47,32 @@ const categories = useCategory()
                   Home
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to="/catagory" className="nav-link">
-                  Catagory
-                </NavLink>
+              
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to={"/categories"}
+                  data-bs-toggle="dropdown"
+                >
+                  Categories
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to={"/categories"}>
+                      All Categories
+                    </Link>
+                  </li>
+                  {categories?.map((c) => (
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to={`/category/${c.slug}`}
+                      >
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </li>
 
               {/* if there is no user show register and login page  */}

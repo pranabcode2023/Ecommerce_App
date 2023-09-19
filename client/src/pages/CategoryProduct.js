bb6c554bd8778a2b7e7f75080a640aa8c9331f96
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-19 17:20:29 +0200

    div wrap with layout

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "../components/Layout/Layout";
 
 const CategoryProduct = () => {
-  return <div>CategoryProduct</div>;
+  return (
+    <Layout>
+      <div className="container">
+        <h1>Category name</h1>
+      </div>
+    </Layout>
+  );
 };
 
 export default CategoryProduct;

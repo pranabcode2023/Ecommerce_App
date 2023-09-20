8d1a2dba4d71167c91d8af410a53f3fd570a72d9
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-20 14:50:31 +0200

    added user profile form  copied from register page to save time as well as added function .

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import UserMenu from "../../components/Layout/UserMenu";
-import Dashboard from "./Dashboard";
-
+import { useAuth } from "../../context/auth";
+import axios from "axios";
+import toast from "react-hot-toast";
 const Profile = () => {
+  //context
+  const [auth, setAuth] = useAuth();
+  //state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  // get user Data
+  useEffect(() => {
+    const { name, email, phone, address } = auth?.user;
+    setName(name);
+    setEmail(email);
+    setPhone(phone);
+    setAddress(address);
+  }, [auth?.user]);
+
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(name, email, password, phone, address, answer);
+    // toast.success("register successfully"); // toast pop up from react toastify
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/register`,
+        {
+          name,
+          email,
+          password,
+          phone,
+          address,
+        }
+      );
+
+      // if (res && res.data.success) {
+      //   toast.success(res && res.data.message);
+      // } else {
+      //   toast.error(res.data.message);
+      // }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <Layout title={"Your Profile "}>
       <div className="container-fluid m-3 p-3 ">
@@ -12,7 +59,73 @@ const Profile = () => {
             <UserMenu />
           </div>
           <div className="col-md-9">
-            <h1> Your Profile </h1>
+            <div className="form-container" style={{ minHeight: "90vh" }}>
+              <form onSubmit={handleSubmit}>
+                <h4 className="title">USER PROFILE</h4>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} // on change event
+                    className="form-control"
+                    id="exampleInputName"
+                    placeholder="Enter your Name"
+                    required //  validation
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter your Email"
+                    required
+                    //NOTE - method used for disable the input field. thats why it's not possible to edit anymore
+                    disabled
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter your password"
+                    required
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter your Phone no."
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <textarea
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter your Address"
+                    required
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  UPDATE
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>

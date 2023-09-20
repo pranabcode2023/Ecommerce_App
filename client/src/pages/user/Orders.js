cfd957c1a9583abe13faca642d7e4dc69f9e835a
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-20 23:35:29 +0200

    maping orders to show on page and function to get orders

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import UserMenu from "../../components/Layout/UserMenu";
-
+import axios from "axios";
+import { useAuth } from "../../context/auth";
+import moment from "moment";
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useAuth();
+  const getOrders = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/auth/orders`
+      );
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (auth?.token) getOrders();
+  }, [auth?.token]);
   return (
     <Layout title={"Yours Orders"}>
       <div className="container-fluid p-3 m-3">
@@ -11,7 +29,41 @@ const Orders = () => {
             <UserMenu />
           </div>
           <div className="col-md-9">
-            <h1>All Orders</h1>
+            {/* //NOTE - checking function working or not  */}
+            {/* <p> {JSON.stringify(orders, null, 4)}</p> */}
+
+            <h1 className="text-center">All Orders</h1>
+
+            {orders?.map((o, i) => {
+              return (
+                <div className="border shadow">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <td scope="col">#</td>
+                        <td scope="col">Status</td>
+                        <td scope="col">Buyer</td>
+                        <td scope="col">Date</td>
+                        <td scope="col">Payment</td>
+                        <td scope="col">Quantity</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th>{i + 1}</th>
+                        <th>{o?.status}</th>
+                        <th>{o?.buyer?.name}</th>
+                        <th>{moment(o?.createdAt).fromNow()}</th>
+
+                        <th>{o?.payment.success ? "Success" : "Failed"}</th>
+                        <th>{o?.products?.length}</th>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container"></div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>

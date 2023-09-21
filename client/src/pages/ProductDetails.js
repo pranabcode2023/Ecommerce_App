9a22997348c8c250775c071a3592b5f94c3a9c7e
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-21 15:05:37 +0200

    refractoring

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,29 +1,35 @@
-import React, {useState, useEffect} from 'react'
-import Layout from '../components/Layout/Layout'
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout/Layout";
+import axios from "axios";
+import { useCart } from "../context/cart";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../styles/ProductDetailsStyles.css";
 const ProductDetails = () => {
-    const params = useParams()
-    const [product, setProduct] = useState({})
-    const [relatedProducts, setRelatedProducts] = useState([]);
-    //intial product details
-    useEffect(()=>{
-        if (params?.slug) getProduct()
-    }, [params?.slug])
-    //get product 
-    const getProduct = async()=> {
-        try {
-            const {data}= await axios.get(
-                `${process.env.REACT_APP_API}/api/v1/product/get-product/${params.slug}`
-              );
-              setProduct(data?.product);
-              getSimilarProduct(data?.product._id, data?.product.category._id);
-        } catch (error) {
-            console.log(error)
-        }
+  const navigate = useNavigate();
+  const params = useParams();
+  const [product, setProduct] = useState({});
+  const [relatedProducts, setRelatedProducts] = useState([]);
+  const [cart, setCart] = useCart();
+  //intial product details
+  useEffect(() => {
+    if (params?.slug) getProduct();
+  }, [params?.slug]);
+  //get product
+  const getProduct = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/get-product/${params.slug}`
+      );
+      setProduct(data?.product);
+      getSimilarProduct(data?.product._id, data?.product.category._id);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-     //get similar product
+  //get similar product
   const getSimilarProduct = async (pid, cid) => {
     try {
       const { data } = await axios.get(
@@ -36,20 +42,19 @@ const ProductDetails = () => {
   };
   return (
     <Layout>
-       
-       {/* for checking function working or not  */}
-        {/* {JSON.stringify(product, null,4)} */}
+      {/* for checking function working or not  */}
+      {/* {JSON.stringify(product, null,4)} */}
 
-        <div className='row container mt-2'>
-            <div className='col-md-6'>
-            <img
-                      src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${product._id}`}
-                      className="card-img-top"
-                      alt={product.name}
-                      height= '500'
-                    />
-            </div>
-            <div className="col-md-6 product-details-info">
+      <div className="row container product-details">
+        <div className="col-md-5 ">
+          <img
+            src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${product._id}`}
+            className="card-img-top"
+            alt={product.name}
+            height="500"
+          />
+        </div>
+        <div className="col-md-7 product-details-info">
           <h1 className="text-center">Product Details</h1>
           <hr />
           <h6>Name : {product.name}</h6>
@@ -66,38 +71,68 @@ const ProductDetails = () => {
         </div>
       </div>
       {/* Horizontal row */}
-      <hr/>
-        <div className='row container'>
-           {/* for checking function working or not  */}
-            {/* {JSON.stringify(relatedProducts, null,4)} */}
-            <h4>Similar Products</h4>
-             {relatedProducts.length <1 && (<p className='text-center'>No Similar Products Found</p>)}
-          <div className="d-flex flex-wrap">
-            {relatedProducts?.map((p) => (
-              <div className="card m-2" style={{ width: "18rem" }}>
-                <img
-                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
-                  className="card-img-top"
-                  alt={p.name}
-                />
-                <div className="card-body">
+      <hr />
+      <div className="row container similar-products">
+        {/* for checking function working or not  */}
+        {/* {JSON.stringify(relatedProducts, null,4)} */}
+        <h4>Similar Products ➡️</h4>
+        {relatedProducts.length < 1 && (
+          <p className="text-center">No Similar Products Found</p>
+        )}
+        <div className="d-flex flex-wrap">
+          {relatedProducts?.map((p) => (
+            <div className="card m-2" style={{ width: "18rem" }}>
+              <img
+                src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                className="card-img-top"
+                alt={p.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{p.name}</h5>
+                <p className="card-text">
+                  {/* substring function used to show maximum 30 character  */}
+                  {p.description.substring(0, 30)}...{" "}
+                </p>
+                {/* <p className="card-text">€ {p.price} </p> */}
+                <div className="card-name-price">
                   <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">
-                    {/* substring function used to show maximum 30 character  */}
-                    {p.description.substring(0, 30)}...{" "}
-                  </p>
-                  <p className="card-text">€ {p.price} </p>
-                 
-                  <button className="btn btn-secondary ms-1">
+                  <h5 className="card-title card-price">
+                    {p.price.toLocaleString("de-EU", {
+                      style: "currency",
+                      currency: "EUR",
+                    })}
+                  </h5>
+                </div>
+
+                {/* <button className="btn btn-secondary ms-1">ADD TO CART</button> */}
+                <div className="card-name-price">
+                  <button
+                    className="btn btn-primary ms-1"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                  >
+                    More Details
+                  </button>
+                  <button
+                    className="btn btn-secondary ms-1"
+                    onClick={() => {
+                      setCart([...cart, p]);
+                      localStorage.setItem(
+                        "cart",
+                        JSON.stringify([...cart, p])
+                      );
+                      toast.success("Iteam added to Cart");
+                    }}
+                  >
                     ADD TO CART
                   </button>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;

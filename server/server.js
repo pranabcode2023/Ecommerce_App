2fd2c4158586c12d30a73a5a05eeb71956d52d37
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-21 21:47:10 +0200

    cors options disable

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ connectDB();
 const app = express();
 
 //middelwares
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -56,7 +56,7 @@ const corsOptions = {
     }
   },
 };
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //run listen
 app.listen(PORT, () => {

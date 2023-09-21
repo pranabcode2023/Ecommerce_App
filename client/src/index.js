c42c3fef8ddfb2fa3cab6cff7d9780b1640785a6
pranabcode2023 <pranabcode2023@gmail.com>
2023-09-21 12:28:53 +0200

    refractoring code

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import "../src/styles/AuthStyles.css";
-// import "../src/styles/CartStyles.css";
-// import "../src/styles/CategoryProductStyles.css";
-// import "../src/styles/HomepageStyles.css";
-// import "../src/styles/ProductDetailsStyles.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";

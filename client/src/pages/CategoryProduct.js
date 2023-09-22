9683d0ba909664a89e41d479e6d6b1bde1e69633
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-09-22 19:30:33 +0200

    put some style on h4 text and changed the classname

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -38,7 +38,7 @@ const CategoryProduct = () => {
   return (
     <Layout>
       <div className="container mt-3 category">
-        <h4 className="text-center"> Category- {category?.name}</h4>
+        <h4 className="text-center text-cat-h4"> Category- {category?.name}</h4>
         <h6 className="text-center"> {products?.length} result found</h6>
         <div className="row">
           <div className="col-md-9 offset-1">

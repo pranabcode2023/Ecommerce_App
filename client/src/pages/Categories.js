f8e277538a9c3c70ce0fc81d4cd46af2897fcc55
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-15 13:02:18 +0100

    put some css style on it

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -7,18 +7,25 @@ const Categories = () => {
   const categories = useCategory();
   return (
     <Layout title={"All Categories"}>
-      <div className="container catagories-page" style={{ marginTop: "50px" }}>
-        <h2 className="text-center">All Categories</h2>
-        <div className="row container">
-          {categories.map((c) => (
-            <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
-              <div className="card">
-                <Link to={`/category/${c.slug}`} className="btn cat-btn">
-                  {c.name}
-                </Link>
+      <div className="catagoryPage">
+        <div className="container catagories-page">
+          <div className="row container">
+            <h2
+              className="card"
+              style={{ marginTop: "20px", textAlign: "center" }}
+            >
+              All Categories
+            </h2>
+            {categories.map((c) => (
+              <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
+                <div className="card">
+                  <Link to={`/category/${c.slug}`} className="btn cat-btn">
+                    {c.name}
+                  </Link>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
